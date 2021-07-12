a869a4458b915204a6f7faadbef1141b9325b005
tobiasraab <tbs.raab@gmail.com>
2021-07-12 09:59:45 +0200

    create Page

---
+++ pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Contact
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'portfolio'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <projectCard></projectCard>
+  </div>
+</template>
+
+<script>
+import projectCard from './../components/projectCard.vue'
+
+export default {
+  name: 'projects',
+  layout: 'portfolio',
+  components: {
+    projectCard
+  }
+}
+</script>
+
+<style>
+
+</style>
