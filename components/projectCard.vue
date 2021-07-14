0ff1eb5795c0df11d7dcad89869af3af5e225fd4
tobiasraab <tbs.raab@gmail.com>
2021-07-14 12:35:42 +0200

    add nuxt link

--- components/projectCard.vue
+++ components/projectCard.vue
@@ -1,106 +1,119 @@
 <template>
+    <NuxtLink class="nuxt-link" :to="{path: this.linkedPath}">
+
   <div class="project-card-container" @mouseenter="mouseEnter()" @mouseleave="mouseLeave()">
-      <img class="project-card-image" v-bind:src="this.image"/>
+      <img class="project-card-image" v-bind:src="this.image" />
       <div class="project-card-text-container pr-4 pt-11 sm:pr-4 sm:pt-10">
         <h2 class="text-xs sm:text-sm">{{this.label}}</h2>
         <h1 class="text-xl sm:text-2xl">{{this.title}}</h1>
       </div>
       <div class="project-card-hover-overlay" v-bind:id="this.overlayId">
-        <p class="project-card-hover-description"
-          v-html="this.description"
-        ></p>
+        <p class="project-card-hover-description" v-html="this.description"></p>
       </div>
   </div>
+    </NuxtLink>
+
 </template>
 
 <script>
-export default {
-  name: 'projectCard',
-  props: ['title', 'label', 'image', 'description', 'overlayId'],
-  methods: {
-    mouseEnter(){
-      document.getElementById(this.overlayId).style.clipPath = 'polygon(0 0% , 100% 0%, 100% 100%, 0 100%)'
-    },
-    mouseLeave(){
-      document.getElementById(this.overlayId).style.clipPath = 'polygon(0 78.64% , 100% 70%, 100% 100%, 0 100%)'
+  export default {
+    name: 'projectCard',
+    props: ['title', 'label', 'image', 'description', 'overlayId', 'linkedPath'],
+    methods: {
+      mouseEnter() {
+        document.getElementById(this.overlayId).style.clipPath = 'polygon(0 0% , 100% 0%, 100% 100%, 0 100%)'
+      },
+      mouseLeave() {
+        document.getElementById(this.overlayId).style.clipPath = 'polygon(0 78.64% , 100% 70%, 100% 100%, 0 100%)'
+      }
     }
   }
-}
+
 </script>
 
 <style>
-  .project-card-container{
-    display:inline-block;
+  .nuxt-link{
+    height: 400px;
+  }
+  .project-card-container {
+    display: inline-block;
 
     height: 400px;
     width: 100%;
 
     margin: auto;
-    
+
     cursor: pointer;
 
     background-color: rgb(214, 214, 214);
     color: white;
   }
-    .project-card-image{
-      z-index: 1;
 
-      position: relative;
+  .project-card-image {
+    z-index: 1;
 
-      height: 85%;
-      width: 100%;
-    }
-    .project-card-text-container{
-      z-index: 3;
+    position: relative;
 
-      position: relative;
-      top: -12%;
-      bottom: 0;
+    height: 85%;
+    width: 100%;
+  }
 
-      height: 27%;
-      width: 100%;
+  .project-card-text-container {
+    z-index: 3;
 
-      /* padding-right: 24px; */
-      /* padding-top: 32px; */
+    position: relative;
+    top: -12%;
+    bottom: 0;
 
-      text-align: right;
+    height: 27%;
+    width: 100%;
 
-      clip-path: polygon(0 32% , 100% 0, 100% 100%, 0 100%);
-      background-color: #1B4E5F;
-    }
-    .project-card-hover-overlay{
-      z-index: 2;
+    /* padding-right: 24px; */
+    /* padding-top: 32px; */
 
-      position: relative;
-      top: -112%;
+    text-align: right;
 
-      height: 100%;
-      width: 100%;
+    clip-path: polygon(0 32%, 100% 0, 100% 100%, 0 100%);
+    background-color: #1B4E5F;
+  }
 
-      padding: 12px;
+  .project-card-hover-overlay {
+    z-index: 2;
 
-      transition-duration: 1s;
-      clip-path: polygon(0 78.64% , 100% 70%, 100% 100%, 0 100%);
-      
-      background-color: rgba(255, 230, 0, 0.8);
-    }
-    .project-card-hover-description{
-      display: block;
+    position: relative;
+    top: -112%;
 
-      padding: 2px;
+    height: 100%;
+    width: 100%;
 
-      font-family: 'silkamedium';
-      font-size: 19px;
-      text-align: right;
+    padding: 12px;
 
-      color: #0b2027;
-    }
-      h1 {
-        font-family: 'silkaregular';
-        font-size: 28px;
-      }
-      h2 {
-        font-family: 'silkalight';
-        font-size: 14px;
-      }
+    transition-duration: 1s;
+    clip-path: polygon(0 78.64%, 100% 70%, 100% 100%, 0 100%);
+
+    background-color: rgba(255, 230, 0, 0.8);
+  }
+
+  .project-card-hover-description {
+    display: block;
+
+    padding: 2px;
+
+    font-family: 'silkamedium';
+    font-size: 19px;
+    text-align: right;
+
+    color: #0b2027;
+  }
+
+  h1 {
+    font-family: 'silkaregular';
+    font-size: 28px;
+  }
+
+  h2 {
+    font-family: 'silkalight';
+    font-size: 14px;
+  }
+
 </style>
