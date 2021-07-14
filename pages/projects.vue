192749197efa9bd96da5628b08fa03cc8786150a
tobiasraab <tbs.raab@gmail.com>
2021-07-14 12:02:50 +0200

    import example projectcards

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <projectCard></projectCard>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16 h-auto max-w-screen-2xl m-auto">
+    <projectCard class="projectcard-tag"
+      :title="'Wärmepumpe'"
+      :label="'UX/UI Design'"
+      :image="require('@/assets/images/testImg1.jpg')"
+      :description="'Das ist die tolle Beschreibung <br> Ich bin krass, gebt mir euer Geld'"
+      :overlayId="'overlayId1'"
+    ></projectCard>
+     <projectCard class="projectcard-tag"
+      :title="'HUBI - Education Kit'"
+      :label="'Smart Home'"
+      :image="require('@/assets/images/testImg2.jpg')"
+      :description="'Das ist die nächste tolle Beschreibung <br> Ich bin krass, gebt mir noch mehr Geld'"
+      :overlayId="'overlayId2'"
+    ></projectCard>
+     <projectCard class="projectcard-tag"
+      :title="'Fire Watch'"
+      :label="'IoT System'"
+      :image="require('@/assets/images/testImg2.jpg')"
+      :description="'Das ist die nächste tolle Beschreibung <br> Ich bin krass, gebt mir noch mehr Geld'"
+      :overlayId="'overlayId3'"
+    ></projectCard>
   </div>
 </template>
 
