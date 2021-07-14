b61650023d231081a8690cd2f7ac2f8cb2223b58
tobiasraab <tbs.raab@gmail.com>
2021-07-14 12:01:53 +0200

    Update projectCard.vue

--- components/projectCard.vue
+++ components/projectCard.vue
@@ -1,16 +1,106 @@
 <template>
-  <div>
-      <h1>Projecttitle</h1>
-      <p>Description</p>
+  <div class="project-card-container" @mouseenter="mouseEnter()" @mouseleave="mouseLeave()">
+      <img class="project-card-image" v-bind:src="this.image"/>
+      <div class="project-card-text-container pr-4 pt-11 sm:pr-4 sm:pt-10">
+        <h2 class="text-xs sm:text-sm">{{this.label}}</h2>
+        <h1 class="text-xl sm:text-2xl">{{this.title}}</h1>
+      </div>
+      <div class="project-card-hover-overlay" v-bind:id="this.overlayId">
+        <p class="project-card-hover-description"
+          v-html="this.description"
+        ></p>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'projectCard',
+  props: ['title', 'label', 'image', 'description', 'overlayId'],
+  methods: {
+    mouseEnter(){
+      document.getElementById(this.overlayId).style.clipPath = 'polygon(0 0% , 100% 0%, 100% 100%, 0 100%)'
+    },
+    mouseLeave(){
+      document.getElementById(this.overlayId).style.clipPath = 'polygon(0 78.64% , 100% 70%, 100% 100%, 0 100%)'
+    }
+  }
 }
 </script>
 
 <style>
+  .project-card-container{
+    display:inline-block;
 
+    height: 400px;
+    width: 100%;
+
+    margin: auto;
+    
+    cursor: pointer;
+
+    background-color: rgb(214, 214, 214);
+    color: white;
+  }
+    .project-card-image{
+      z-index: 1;
+
+      position: relative;
+
+      height: 85%;
+      width: 100%;
+    }
+    .project-card-text-container{
+      z-index: 3;
+
+      position: relative;
+      top: -12%;
+      bottom: 0;
+
+      height: 27%;
+      width: 100%;
+
+      /* padding-right: 24px; */
+      /* padding-top: 32px; */
+
+      text-align: right;
+
+      clip-path: polygon(0 32% , 100% 0, 100% 100%, 0 100%);
+      background-color: #1B4E5F;
+    }
+    .project-card-hover-overlay{
+      z-index: 2;
+
+      position: relative;
+      top: -112%;
+
+      height: 100%;
+      width: 100%;
+
+      padding: 12px;
+
+      transition-duration: 1s;
+      clip-path: polygon(0 78.64% , 100% 70%, 100% 100%, 0 100%);
+      
+      background-color: rgba(255, 230, 0, 0.8);
+    }
+    .project-card-hover-description{
+      display: block;
+
+      padding: 2px;
+
+      font-family: 'silkamedium';
+      font-size: 19px;
+      text-align: right;
+
+      color: #0b2027;
+    }
+      h1 {
+        font-family: 'silkaregular';
+        font-size: 28px;
+      }
+      h2 {
+        font-family: 'silkalight';
+        font-size: 14px;
+      }
 </style>
