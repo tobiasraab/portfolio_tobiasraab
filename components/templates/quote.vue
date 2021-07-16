5a6757ed464ab10cbea993c4b3ea41b95fa747f1
tobiasraab <tbs.raab@gmail.com>
2021-07-16 12:46:05 +0200

    create template quote

---
+++ components/templates/quote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="quote-container"
+      v-bind:style="{backgroundColor: this.backgroundColor, color: this.color}"
+      v-html="this.text"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quote',
+  props: ['backgroundColor', 'color', 'text']
+  
+}
+</script>
+
+<style>
+  .quote-container{
+    display: table-cell;
+    vertical-align: middle;
+
+    position: relative;
+    width: 100vw;
+    height: 40vh;
+
+    text-align: center;
+
+    font-family: 'silkaextra_light';
+    font-size: 32px;
+  }
+</style>
