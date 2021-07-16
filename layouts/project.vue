6b2c2624784330675363dc9ac05301dd36e77ba9
tobiasraab <tbs.raab@gmail.com>
2021-07-16 12:45:31 +0200

    Create template project

---
+++ layouts/project.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <navigationBar class="nav-bar"/>
+    <Nuxt class="layoutNuxt p-0 pt-12" />
+  </div>
+</template>
+
+<script>
+import navigationBar from './../components/navigationBar.vue'
+
+export default {
+    components: {
+        navigationBar
+    }
+}
+</script>
+<style>
+@import './../assets/fonts/fontRoman.css';
+@import './../assets/fonts/fontitalic.css';
+  body {
+    font-family: 'silkaregular';
+  }
+  .layoutNuxt {
+    z-index: 1;
+  }
+  .nav-bar{
+    z-index: 10;
+  }
+</style>
