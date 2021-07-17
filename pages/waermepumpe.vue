c66f4c6f2b7cb245ac817fe4dfe3e9a64493deab
tobiasraab <tbs.raab@gmail.com>
2021-07-17 21:03:15 +0200

    Update waermepumpe.vue

--- pages/waermepumpe.vue
+++ pages/waermepumpe.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-    <quote
+
+    <quote class="mt-16 mb-16"
       :backgroundColor="'#000000'"
       :color="'#ffffff'"
-      :text="'Das ist sehr inspirierend'"
+      :text="'Dashboard für Wärmepumpen'"
     ></quote>
+    <textPicture
+      :imageSource="require('@/assets/images/waermepumpe/image3.png')"
+      :headline="'Need'"
+      :text="'Das entworfene Dashboard hilft dem Nutzer seinen Energiehaushalt im Auge zu behalten. Er bekommt historische Daten zur Effektifität seiner Wärmepumpe angezeigt usw.'"
+      :backgroundColor="'#ffffff'"
+      :imageCaption="''"
+    ></textPicture>
+    <pictureText
+      :imageSource="require('@/assets/images/waermepumpe/image3.png')"
+      :headline="'Need'"
+      :text="'Das entworfene Dashboard hilft dem Nutzer seinen Energiehaushalt im Auge zu behalten. Er bekommt historische Daten zur Effektifität seiner Wärmepumpe angezeigt usw.'"
+      :backgroundColor="'#ffffff'"
+      :imageCaption="''"
+    ></pictureText>
   </div>
 </template>
 
 <script>
 import quote from './../components/templates/quote.vue'
+import textPicture from './../components/templates/textPicture.vue'
+import pictureText from './../components/templates/pictureText.vue'
+
 export default {
   name: 'waermepumpe',
   layout: 'project',
   components: {
-    quote
+    quote,
+    textPicture,
+    pictureText
   }
 }
 </script>
