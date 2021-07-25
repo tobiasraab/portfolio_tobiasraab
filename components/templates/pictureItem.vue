c3e518549ae80eb83adedc3e4406602d5d243c70
tobiasraab <tbs.raab@gmail.com>
2021-07-25 14:33:07 +0200

    created to rename picture.vue

---
+++ components/templates/pictureItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-12 m-auto max-w-screen-2xl">
+    <h2>{{this.headline}}</h2>
+    <figure>
+      <img v-bind:src="this.imageSource" />
+      <figcaption>{{this.imageCaption}}</figcaption>
+    </figure>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pictureitem',
+    props: ['headline', 'imageSource', 'imageCaption']
+  }
+
+</script>
+
+<style scoped>
+  div > h2 {
+    font-family: 'silkamedium';
+    font-size: 32px;
+    color: #061216;
+    margin-bottom: 32px;
+  }
+</style>
