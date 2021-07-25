9495e72f34cd93bc8de3852033f40dabd0f7ec1a
tobiasraab <tbs.raab@gmail.com>
2021-07-25 14:37:06 +0200

    Create firewatch.vue

---
+++ pages/projects/firewatch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+     <quote class="mt-16 mb-32"
+      :backgroundColor="'#161616'"
+      :color="'#ffffff'"
+      :text="'Camp Creek Road Fire:<br>85 Tote'"
+      :origin="'Quelle: Government of California (CAL FIRE)'"
+    ></quote>
+  </div>
+</template>
+
+<script>
+import quote from '~/components/templates/quote.vue'
+import pictureText from '~/components/templates/pictureText.vue'
+import textPicture from '~/components/templates/textPicture.vue'
+import productValues from '~/components/templates/productValues.vue'
+import textParagraph from '~/components/templates/textParagraph.vue'
+
+
+export default {
+  name: 'firewatch',
+  layout: 'project',
+  components: {
+    quote,
+    pictureText,
+    productValues,
+    textPicture,
+    textParagraph
+  }
+}
+</script>
+
+<style scoped>
+</style>
