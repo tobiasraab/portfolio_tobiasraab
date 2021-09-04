5a50e2f59f4f74d8c734f0f8b4bef91459870ffb
tobiasraab <tbs.raab@gmail.com>
2021-09-04 22:28:25 +0200

    Update navigationBar.vue

--- components/navigationBar.vue
+++ components/navigationBar.vue
@@ -6,13 +6,23 @@
       <div class="hidden sm:flex justify-between">
         <div class="hidden sm:flex justify-between" id="navTopLinksContainer">
           <NuxtLink class="nav-top-link menuLink" to="/">About</NuxtLink>
-          <NuxtLink class="nav-top-link menuLink projects" to="/projects">Projects</NuxtLink>
-          <NuxtLink class="nav-top-link menuLink" to="/contact">Contact</NuxtLink>
+          <NuxtLink class="nav-top-link menuLink projects" to="/projects"
+            >Projects</NuxtLink
+          >
+          <NuxtLink class="nav-top-link menuLink" to="/contact"
+            >Contact</NuxtLink
+          >
         </div>
-        <h3 class="navTopMenu-currentSite hidden md:block">{{this.bigScreenCurrentSite}}</h3>
+        <h3 class="navTopMenu-currentSite hidden md:block">
+          {{ this.bigScreenCurrentSite }}
+        </h3>
       </div>
       <!--menu icon if screen < sm -->
-      <div class="inline-flex sm:hidden" id="navMenuIconContainer" v-on:click="sideMenu">
+      <div
+        class="inline-flex sm:hidden"
+        id="navMenuIconContainer"
+        v-on:click="sideMenu"
+      >
         <img class="nav-toggle-icon" src="./../assets/icons/navbarIcon.svg" />
       </div>
       <!-- current link if screen < sm -->
@@ -22,232 +32,267 @@
     <div class="block sm:hidden navActiveSideLink" id="navSideMenu">
       <!-- links if screen < sm-->
       <div id="navSideLinksContainer">
-        <NuxtLink class="nav-side-link menuLink" id="navSideLinkAbout" to="/">About</NuxtLink>
-        <NuxtLink class="nav-side-link menuLink projects" id="navSideLinkProjects" to="/projects">Projects</NuxtLink>
-        <NuxtLink class="nav-side-link menuLink" id="navSideLinkContact" to="/contact">Contact</NuxtLink>
+        <NuxtLink class="nav-side-link menuLink" id="navSideLinkAbout" to="/"
+          >About</NuxtLink
+        >
+        <NuxtLink
+          class="nav-side-link menuLink projects"
+          id="navSideLinkProjects"
+          to="/projects"
+          >Projects</NuxtLink
+        >
+        <NuxtLink
+          class="nav-side-link menuLink"
+          id="navSideLinkContact"
+          to="/contact"
+          >Contact</NuxtLink
+        >
       </div>
     </div>
   </header>
 </template>
 
 <script>
-  export default {
-    name: "navigationBar",
-    data() {
-      return {
-        navSideMenuActiv: false,
-        currentSite: this.$router.currentRoute.path,
-        bigScreenCurrentSite: undefined
-      };
-    },
-    mounted() {
-      this.bigScreenCurrentSite = undefined
-      if (this.currentSite == "/projects/waermepumpe") {
-        this.currentSite = "/projects/Wärmepumpe";
-        this.bigScreenCurrentSite = "Wärmepumpe"
-      } else if (this.currentSite == "/projects/hubi") {
-        this.currentSite = "/projects/Hubi";
-        this.bigScreenCurrentSite = "HUBI"
-      } else if (this.currentSite == "/projects/firewatch") {
-        this.currentSite = "/projects/Firewatch";
-        this.bigScreenCurrentSite = "Firewatch"
-      } else if (this.currentSite == "/projects/pflege") {
-        this.currentSite = "/projects/Pflege";
-        this.bigScreenCurrentSite = "Pflege"
-      }
-      else if(this.bigScreenCurrentSite == "/projects" || "/about" || "/contact" || "/"){
-        this.bigScreenCurrentSite = ""
-      }
+export default {
+  name: "navigationBar",
+  data() {
+    return {
+      navSideMenuActiv: false,
+      currentSite: this.$router.currentRoute.path,
+      bigScreenCurrentSite: undefined,
+    };
+  },
+  mounted() {
+    this.bigScreenCurrentSite = undefined;
+    if (this.currentSite == "/projects/waermepumpe") {
+      this.currentSite = "/projects/Wärmepumpe";
+      this.bigScreenCurrentSite = "Wärmepumpe";
+    } else if (this.currentSite == "/projects/hubi") {
+      this.currentSite = "/projects/Hubi";
+      this.bigScreenCurrentSite = "HUBI";
+    } else if (this.currentSite == "/projects/firewatch") {
+      this.currentSite = "/projects/Firewatch";
+      this.bigScreenCurrentSite = "Firewatch";
+    } else if (this.currentSite == "/projects/pflege") {
+      this.currentSite = "/projects/Pflege";
+      this.bigScreenCurrentSite = "Pflege";
+    } else if (
+      this.bigScreenCurrentSite == "/projects" ||
+      "/about" ||
+      "/contact" ||
+      "/"
+    ) {
+      this.bigScreenCurrentSite = "";
+    }
 
+    const icon = document.getElementById("navMenuIconContainer");
+    const sideMenu = document.getElementById("navSideMenu");
+    const sideMenuLinkContainer = document.getElementById(
+      "navSideLinksContainer"
+    );
+
+    window.addEventListener("resize", (e) => {
+      if (window.innerWidth >= 420) {
+        this.navSideMenuActiv = false;
+
+        sideMenu.style.height = "0px";
+        sideMenuLinkContainer.style.display = "none";
+
+        icon.style.transform = "rotate(0deg)";
+        icon.style.paddingTop = "12px";
+      } else if (window.innerWidth < 420) {
+        sideMenuLinkContainer.style.display = "block";
+
+        if (this.navSideMenuActiv === false) {
+          icon.style.transform = "rotate(0deg)";
+          icon.style.paddingTop = "12px";
+        } else if (this.navSideMenuActiv === true) {
+          icon.style.transform = "rotate(90deg)";
+          icon.style.paddingTop = "16px";
+        }
+      }
+    });
+  },
+  methods: {
+    sideMenu() {
       const icon = document.getElementById("navMenuIconContainer");
       const sideMenu = document.getElementById("navSideMenu");
       const sideMenuLinkContainer = document.getElementById(
         "navSideLinksContainer"
       );
 
-      window.addEventListener("resize", (e) => {
-        if (window.innerWidth >= 420) {
-          this.navSideMenuActiv = false;
+      if (this.navSideMenuActiv === false) {
+        this.navSideMenuActiv = true;
 
-          sideMenu.style.height = "0px";
-          sideMenuLinkContainer.style.display = "none";
+        sideMenu.style.height = window.innerHeight - 64 - 24 + "px";
 
-          icon.style.transform = "rotate(0deg)";
-          icon.style.paddingTop = "12px";
-        } else if (window.innerWidth < 420) {
-          sideMenuLinkContainer.style.display = "block";
+        icon.style.transform = "rotate(90deg)";
+        icon.style.paddingTop = "16px";
+      } else if (this.navSideMenuActiv === true) {
+        this.navSideMenuActiv = false;
 
-          if (this.navSideMenuActiv === false) {
-            icon.style.transform = "rotate(0deg)";
-            icon.style.paddingTop = "12px";
-          } else if (this.navSideMenuActiv === true) {
-            icon.style.transform = "rotate(90deg)";
-            icon.style.paddingTop = "16px";
-          }
-        }
-      });
+        sideMenu.style.height = "0px";
+
+        icon.style.transform = "rotate(0deg)";
+        icon.style.paddingTop = "12px";
+      }
     },
-    methods: {
-      sideMenu() {
-        const icon = document.getElementById("navMenuIconContainer");
-        const sideMenu = document.getElementById("navSideMenu");
-        const sideMenuLinkContainer = document.getElementById(
-          "navSideLinksContainer"
-        );
+  },
+  watch: {
+    $route: function () {
+      this.bigScreenCurrentSite = undefined;
+      this.currentSite = this.$router.currentRoute.path;
 
-        if (this.navSideMenuActiv === false) {
-          this.navSideMenuActiv = true;
-
-          sideMenu.style.height = window.innerHeight - 64 - 24 + "px";
-
-          icon.style.transform = "rotate(90deg)";
-          icon.style.paddingTop = "16px";
-        } else if (this.navSideMenuActiv === true) {
-          this.navSideMenuActiv = false;
-
-          sideMenu.style.height = "0px";
-
-          icon.style.transform = "rotate(0deg)";
-          icon.style.paddingTop = "12px";
-        }
-      },
+      if (this.currentSite == "/projects/waermepumpe") {
+        this.currentSite = "Wärmepumpe";
+        this.bigScreenCurrentSite = "Wärmepumpe";
+      } else if (this.currentSite == "/projects/hubi") {
+        this.currentSite = "/projects/Hubi";
+        this.bigScreenCurrentSite = "HUBI";
+      } else if (this.currentSite == "/projects/firewatch") {
+        this.currentSite = "/projects/Firewatch";
+        this.bigScreenCurrentSite = "Firewatch";
+      } else if (this.currentSite == "/projects/pflege") {
+        this.currentSite = "/projects/Pflege";
+        this.bigScreenCurrentSite = "Pflege";
+      } else if (
+        this.bigScreenCurrentSite == "/projects" ||
+        "/about" ||
+        "/contact" ||
+        "/"
+      ) {
+        this.bigScreenCurrentSite = "";
+      }
+      console.log("changed route to", this.currentSite);
     },
-    watch: {
-      $route: function () {
-        this.bigScreenCurrentSite = undefined
-        this.currentSite = this.$router.currentRoute.path;
-        console.log("changed route to", this.currentSite);
-      },
-    },
-  };
-
+  },
+};
 </script>
 
 <style scoped>
-  .navTopMenu-currentSite{
-    color: white;
-    font-family: "silkaregular";
-    font-size: 18px;
-    line-height: 64px;
-    margin-right: 48px;
-  }
-  header {
-    display: inline-flex;
+.navTopMenu-currentSite {
+  color: white;
+  font-family: "silkamedium";
+  font-size: 18px;
+  line-height: 64px;
+  margin-right: 48px;
+}
+header {
+  display: inline-flex;
 
-    position: fixed;
-    top: 0%;
+  position: fixed;
+  top: 0%;
 
-    height: 64px;
-    width: 100vw;
-  }
+  height: 64px;
+  width: 100vw;
+}
 
-  #navTopMenu {
-    z-index: 2;
+#navTopMenu {
+  z-index: 2;
 
-    /* position: sticky;
+  /* position: sticky;
       position: -webkit-sticky; */
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    background-color: #0b2027;
-  }
+  background-color: #0b2027;
+}
 
-  #navTopLinksContainer {
-    padding: 0 16px;
-  }
+#navTopLinksContainer {
+  padding: 0 16px;
+}
 
-  .nav-top-link {
-    height: 64px;
-    padding: 0 16px;
+.nav-top-link {
+  height: 64px;
+  padding: 0 16px;
 
-    font-family: "silkaregular";
-    font-size: 18px;
-    line-height: 64px;
+  font-family: "silkaregular";
+  font-size: 18px;
+  line-height: 64px;
 
-    color: white;
-  }
+  color: white;
+}
 
-  .menuLink:hover {
-    color: #ffe600;
-  }
+.menuLink:hover {
+  color: #ffe600;
+}
 
-  #navMenuIconContainer {
-    z-index: 3;
+#navMenuIconContainer {
+  z-index: 3;
 
-    height: 100%;
-    width: 64px;
+  height: 100%;
+  width: 64px;
 
-    margin-left: 8px;
-    padding: 12px;
+  margin-left: 8px;
+  padding: 12px;
 
-    transition-duration: 1s;
-  }
+  transition-duration: 1s;
+}
 
-  .nav-toggle-icon {
-    height: 40px;
-  }
+.nav-toggle-icon {
+  height: 40px;
+}
 
-  h1 {
-    z-index: 3;
+h1 {
+  z-index: 3;
 
-    position: absolute;
+  position: absolute;
 
-    height: 100%;
-    width: 64px;
+  height: 100%;
+  width: 64px;
 
-    margin-left: 8px;
+  margin-left: 8px;
 
-    font-family: "silkaregular";
-    font-size: 18px;
-    line-height: 64px;
+  font-family: "silkaregular";
+  font-size: 18px;
+  line-height: 64px;
 
-    color: white;
-  }
+  color: white;
+}
 
-  #navSideMenu {
-    position: absolute;
-    left: 0%;
-    top: 64px;
+#navSideMenu {
+  position: absolute;
+  left: 0%;
+  top: 64px;
 
-    height: 0px;
-    width: 80vw;
+  height: 0px;
+  width: 80vw;
 
-    transition-duration: 1s;
+  transition-duration: 1s;
 
-    background-color: #0b2027;
-  }
+  background-color: #0b2027;
+}
 
-  #navSideLinksContainer {
-    z-index: 1;
-    display: block;
+#navSideLinksContainer {
+  z-index: 1;
+  display: block;
 
-    position: absolute;
-    bottom: 0px;
+  position: absolute;
+  bottom: 0px;
 
-    margin-left: 24px;
-    margin-bottom: 24px;
-  }
+  margin-left: 24px;
+  margin-bottom: 24px;
+}
 
-  .nav-side-link {
-    z-index: 1;
-    display: block;
+.nav-side-link {
+  z-index: 1;
+  display: block;
 
-    height: 64px;
+  height: 64px;
 
-    font-family: "silkaregular";
-    font-size: 18px;
-    line-height: 64px;
+  font-family: "silkaregular";
+  font-size: 18px;
+  line-height: 64px;
 
-    color: white;
-  }
+  color: white;
+}
 
-  a.nuxt-link-exact-active {
-    color: #ffe600;
-  }
+a.nuxt-link-exact-active {
+  color: #ffe600;
+}
 
-   a.nuxt-link-active.projects {
-    color: #ffe600;
-  }
-
+a.nuxt-link-active.projects {
+  color: #ffe600;
+}
 </style>
