bd2fd842cf78e9ba00b4aa18d4ae36dde82e681b
tobiasraab <tbs.raab@gmail.com>
2021-09-04 22:25:47 +0200

    Change Layout + transparent bg

--- pages/projects/waermepumpe.vue
+++ pages/projects/waermepumpe.vue
@@ -6,20 +6,24 @@
       :color="'#ffffff'"
       :text="'Dashboard für Wärmepumpen'"
     ></quote>
+    <pictureItem
+      class="mb-32"
+      :headline="''"
+      :imageSource="require('@/assets/images/waermepumpe/image1.png')"
+      :imageCaption="''"
+    ></pictureItem>
     <textPicture
       class="mb-32"
       :headline="'Need'"
-      :imageSource="require('@/assets/images/waermepumpe/image1.png')"
+      :imageSource="require('@/assets/images/waermepumpe/image6.png')"
       :text="'Das entworfene Dashboard hilft dem Nutzer seinen Energiehaushalt im Auge zu behalten. Er bekommt historische Daten zur Effektifität seiner Wärmepumpe angezeigt usw.'"
-      :backgroundColor="'#ffffff'"
       :imageCaption="''"
     ></textPicture>
     <pictureText
       class="mb-32"
       :headline="'Energieumsatz'"
-      :imageSource="require('@/assets/images/waermepumpe/image6.jpg')"
+      :imageSource="require('@/assets/images/waermepumpe/image2.png')"
       :text="'In einer Wärmepumpe wird mittels Strom Wärmeenergie gefördert. Je mehr Wärmeenergie aus dem eingespeisten Strom resultiert, desto effizienter ist die Wärmepumpe. Die Ansicht bietet einen intuitiven und klaren Überblick über die verschiedenen Faktoren. In der oberen Hälfte der Darstellung wird der eingespeiste Strom in kWh angegeben. Die Effektivität der Wärmepumpe wird in dem Faktor gemessen, um den sich die eingespeißte Energie erhöht. Dieser wird zwischen den zwei Säulendiagrammen angegeben. In der unteren Hälfte wird die gewonnene Wärmeenergie in rot visualisert.'"
-      :backgroundColor="'#ffffff'"
       :imageCaption="''"
     ></pictureText>
     <pictureText
@@ -27,7 +31,6 @@
       :headline="'Temperatur'"
       :imageSource="require('@/assets/images/waermepumpe/image5.png')"
       :text="'Die Außen- bzw. Bodentemperatur spielt für den Umsatz, den die Wärmepumpe erziehlt, eine wichtige Rolle. Aufgrund dessen kann der Nutzer sich historische Temperaturdaten in seinem Dashboard anzeigen lassen. So kann dieser, aufgrund der Daten aus den vorherigen Jahren, besser abschätzen, wie viel Kosten die Wärmepumpe nächsten Winter vorraussichtlich verursachen wird.'"
-      :backgroundColor="'#ffffff'"
       :imageCaption="''"
     ></pictureText>
     <pictureItem
@@ -42,12 +45,6 @@
       :imageSource="require('@/assets/images/waermepumpe/image7.png')"
       :imageCaption="''"
     ></pictureItem>
-    <pictureItem
-      class="mb-32"
-      :headline="''"
-      :imageSource="require('@/assets/images/waermepumpe/image1.png')"
-      :imageCaption="''"
-    ></pictureItem>
   </div>
 </template>
 
@@ -59,7 +56,7 @@ import pictureItem from "../../components/templates/pictureItem.vue";
 
 export default {
   name: "waermepumpe",
-  layout: "project",
+  layout: "layout-red",
   components: {
     quote,
     textPicture,
