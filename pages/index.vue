4d20cc3806ae3c741d81bbf3918ee825cd8fe7f9
tobiasraab <tbs.raab@gmail.com>
2021-09-04 22:24:47 +0200

    support mobile
    change full stack diagram on mobile devices

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="pt-0">
+    <fullStackDiagram></fullStackDiagram>
     <textPicture
       class="mb-32"
       :headline="'Tobias Raab'"
-      :imageSource="require('@/assets/images/hubi/question.png')"
-      :text="'Ich bin Designer und Creative Technologist aus Schwäbisch Gmünd. Aktuell studiere ich an der Hochschule für Gestaltung Schwäbisch Gmünd \'Internet der Dinge - Gestaltung vernetzter Systeme B.A.\''"
-      :backgroundColor="'#ffffff'"
+      :imageSource="require('@/assets/images/about/test.jpg')"
+      :text="'Ich bin IoT-Designer und Creative Technologist aus Schwäbisch Gmünd. Aktuell studiere ich an der Hochschule für Gestaltung Schwäbisch Gmünd \'Internet der Dinge - Gestaltung vernetzter Systeme B.A.\''"
       :imageCaption="''"
+      :altTag="''"
     ></textPicture>
-    <fullStackDiagram></fullStackDiagram>
-    <background></background>
+    <iconPattern
+      class="block md:hidden"
+      :headTitle="'Design meets Technology'"
+      :title1="'Research'"
+      :icon1="require('@/assets/icons/microscope.svg')"
+      :title2="'UX Design'"
+      :icon2="require('@/assets/icons/touch--interaction.svg')"
+      :title3="'UI Design'"
+      :icon3="require('@/assets/icons/dashboard.svg')"
+      :title4="'Frontend'"
+      :icon4="require('@/assets/icons/code2.svg')"
+      :title5="'Backend'"
+      :icon5="require('@/assets/icons/deployment-pattern.svg')"
+      :title6="'Database'"
+      :icon6="require('@/assets/icons/tree-view--alt.svg')"
+      :title7="'Hardware'"
+      :icon7="require('@/assets/icons/bot.svg')"
+    ></iconPattern>
   </div>
 </template>
 
@@ -17,25 +34,19 @@
 import fullStackDiagram from "~/components/fullStackDiagram.vue";
 import textParagraph from "~/components/templates/textParagraph.vue";
 import textPicture from "~/components/templates/textPicture.vue";
-import background from "~/components/templates/background.vue"
+import iconPattern from "~/components/templates/iconPattern.vue"
 
 export default {
   name: "about",
-  layout: "project",
+  layout: "layout-blue",
   components: {
     fullStackDiagram,
     textParagraph,
     textPicture,
-    background
+    iconPattern
   },
 };
 </script>
 
 <style scoped>
-#siteFooterAbout {
-  display: block;
-  position: relative;
-  left: 0%;
-  margin-top: 96px;
-}
 </style>
