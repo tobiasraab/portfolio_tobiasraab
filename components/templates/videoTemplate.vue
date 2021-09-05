32a5d13096f5a3cd4187407aae9d92c5c4b7d1e8
tobiasraab <tbs.raab@gmail.com>
2021-09-05 20:17:32 +0200

    add TODO

--- components/templates/videoTemplate.vue
+++ components/templates/videoTemplate.vue
@@ -1,4 +1,6 @@
 <template>
+<!-- TODO: -->
+<!-- Use Video tag not youtube!!!!!! -->
   <div class="px-4 sm:px-8 md:px-16 mb-32 md:mb-48 m-auto max-w-screen-2xl video-template">
     <h2 class="text-2xl md:text-3xl mb-4 md:mb-8">{{ this.headline }}</h2>
     <div class="video-container">
